50e191e920435d810c7e1bb688115ae986ef0937
Frederik Batuna <fbatuna@key-biz.it>
2021-12-02 00:28:10 +0100

    fix: immutable map and set

--- src/lib/immutable/Map.ts
+++ src/lib/immutable/Map.ts
@@ -2,25 +2,31 @@ import Immutable from "immutable";
 
 export class Map<Key, Value> {
   private constructor(private immutableMap: Immutable.Map<any, Value>) {}
+  private serializeKey(value: Key): Immutable.Collection<unknown, unknown> {
+    return Immutable.fromJS(value);
+  }
+  private deserializeKey(value: Immutable.Collection<unknown, unknown>): Key {
+    return value.toJS() as any;
+  }
   static empty<Key, Value>() {
     return new Map<Key, Value>(Immutable.Map());
   }
   has(key: Key): boolean {
-    return this.immutableMap.has(Immutable.fromJS(key));
+    return this.immutableMap.has(this.serializeKey(key));
   }
   get(key: Key): Value | undefined {
-    return this.immutableMap.get(Immutable.fromJS(key));
+    return this.immutableMap.get(this.serializeKey(key));
   }
   set(key: Key, value: Value): Map<Key, Value> {
-    return new Map(this.immutableMap.set(Immutable.fromJS(key), value));
+    return new Map(this.immutableMap.set(this.serializeKey(key), value));
   }
   del(key: Key): Map<Key, Value> {
-    return new Map(this.immutableMap.remove(Immutable.fromJS(key)));
+    return new Map(this.immutableMap.remove(this.serializeKey(key)));
   }
   filter(criteria: (key: Key, value: Value) => boolean): Map<Key, Value> {
-    return new Map(this.immutableMap.filter((value, key) => criteria(Immutable.fromJS(key) as any, value)));
+    return new Map(this.immutableMap.filter((value, key) => criteria(this.deserializeKey(key), value)));
   }
   mapValues(mapper: (key: Key, value: Value) => Value): Map<Key, Value> {
-    return new Map(this.immutableMap.map((value, key) => mapper(Immutable.fromJS(key) as any, value)));
+    return new Map(this.immutableMap.map((value, key) => mapper(this.deserializeKey(key), value)));
   }
 }
--- src/lib/immutable/Set.ts
+++ src/lib/immutable/Set.ts
@@ -1,23 +1,29 @@
 import Immutable from "immutable";
 
 export class Set<Value> {
-  private constructor(private immutableSet: Immutable.Set<any>) {}
+  private constructor(private immutableSet: Immutable.Set<unknown>) {}
+  private serializeValue(value: Value): Immutable.Collection<unknown, unknown> {
+    return Immutable.fromJS(value);
+  }
+  private deserializeValue(value: Immutable.Collection<unknown, unknown>): Value {
+    return value.toJS() as any;
+  }
   static empty<Value>() {
     return new Set<Value>(Immutable.Set());
   }
   has(value: Value): boolean {
-    return this.immutableSet.has(Immutable.fromJS(value));
+    return this.immutableSet.has(this.serializeValue(value));
   }
   add(value: Value): Set<Value> {
-    return new Set(this.immutableSet.add(Immutable.fromJS(value)));
+    return new Set(this.immutableSet.add(this.serializeValue(value)));
   }
   del(value: Value): Set<Value> {
-    return new Set(this.immutableSet.remove(Immutable.fromJS(value)));
+    return new Set(this.immutableSet.remove(this.serializeValue(value)));
   }
   filter(criteria: (value: Value) => boolean): Set<Value> {
-    return new Set(this.immutableSet.filter(criteria));
+    return new Set(this.immutableSet.filter((value) => criteria(this.deserializeValue(value as any))));
   }
   toArray(): Array<Value> {
-    return this.immutableSet.toArray().map((value) => Immutable.fromJS(value)) as any;
+    return this.immutableSet.toArray().map((value) => this.serializeValue(value as any)) as any;
   }
 }
